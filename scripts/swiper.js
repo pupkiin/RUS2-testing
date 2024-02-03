1fcc18d5c4dc01255c4147e536ae8aca192376fb
Danila <danila.pupyshev@gmail.com>
2024-02-03 22:10:07 +0300

    add configuration for swiper for two sliders

---
+++ scripts/swiper.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  effect: 'slide',
+
+  simulateTouch: false,
+
+  slidesPerView: 4.4,
+
+  spaceBetween: 20,
+
+  autoplay: {
+    delay: 2000,
+  },
+
+  speed: 2500,
+
+  initialSlide: 1,
+});
